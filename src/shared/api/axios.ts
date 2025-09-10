9f2ca641db2060c2ab44ec4f1f979cab5aaf7fc6
electrohyun <dev.electrohyun@gmail.com>
2025-09-10 16:11:10 +0900

    feat: (#1) 공통 UI 컴포넌트 임시 구현과 Axios 유틸 설정

---
+++ src/shared/api/axios.ts
@@ -0,0 +1,92 @@
+import axios from 'axios';
+import type { AxiosRequestConfig, AxiosError } from 'axios';
+import { requestRefreshToken } from '@/features/auth/api/auth';
+import queryString from 'query-string';
+
+interface APIRequest extends Omit<AxiosRequestConfig, 'data' | 'method'> {
+  url: string;
+  params?: Record<string, any>;
+}
+
+interface APIRequestWithData<D = any> extends APIRequest {
+  data?: D;
+}
+
+interface RetryConfig extends AxiosRequestConfig {
+  _retry?: boolean;
+}
+
+const paramsSerializer = (params: Record<string, any>) =>
+  queryString.stringify(params, { skipEmptyString: true, skipNull: true });
+
+const common: AxiosRequestConfig = {
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  paramsSerializer,
+};
+
+const instance = axios.create(common);
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+
+  const isAuthPath =
+    config.url?.includes('auth/login') || config.url?.includes('auth/signup');
+
+  const cleanToken = token ? token.replace(/"/g, '') : null;
+
+  if (cleanToken && !isAuthPath)
+    config.headers.Authorization = `Bearer ${cleanToken}`;
+
+  return config;
+});
+
+instance.interceptors.response.use(
+  (res) => res,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as RetryConfig;
+
+    const isAuthPath =
+      originalRequest.url?.includes('auth/login') ||
+      originalRequest.url?.includes('auth/signup');
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !isAuthPath
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await requestRefreshToken();
+
+        if (!res.success) {
+          console.error(res.data.message);
+          return Promise.reject(new Error('토큰 갱신 실패'));
+        }
+
+        const newAccessToken = res.data.content;
+        localStorage.setItem('accessToken', newAccessToken);
+
+        if (!originalRequest.headers) originalRequest.headers = {};
+        const cleanNewToken = newAccessToken.replace(/"/g, '');
+        originalRequest.headers.Authorization = `Bearer ${cleanNewToken}`;
+
+        return instance(originalRequest);
+      } catch (err) {
+        console.error('리프레시 실패');
+
+        localStorage.clear();
+
+        window.location.href = '/login';
+
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export { instance };
+export type { APIRequest, APIRequestWithData };
---
+++ src/shared/ui/Spinner.tsx
@@ -0,0 +1,38 @@
+import { motion } from 'framer-motion';
+import clsx from 'clsx';
+
+interface SpinnerProps {
+  message?: string;
+}
+
+const Spinner = ({ message }: SpinnerProps) => {
+  return (
+    <div
+      className={clsx(
+        'flex',
+        'flex-col',
+        'justify-center',
+        'items-center',
+        'py-10',
+      )}
+    >
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1, ease: 'linear' }}
+        className={clsx(
+          'w-8',
+          'h-8',
+          'border-4',
+          'border-t-transparent',
+          'border-blue-500',
+          'rounded-full',
+        )}
+      />
+      {message && (
+        <p className={clsx('mt-2', 'text-gray-500', 'text-sm')}>{message}</p>
+      )}
+    </div>
+  );
+};
+
+export default Spinner;
