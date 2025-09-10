40684a2634d18ccba9c6670fdb7747633767d866
electrohyun <dev.electrohyun@gmail.com>
2025-09-10 16:06:45 +0900

    chore: (#1) Vite + TypeScript로 프로젝트 초기화

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import imagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminSvgo from 'imagemin-svgo';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    imagemin({
+      plugins: {
+        svg: imageminSvgo(),
+      },
+      include: ['**/*.{jpg,jpeg,png,svg}'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
