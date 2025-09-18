ad9319e38c1e603bc4fdf4c605b08af5ebcf8a58
electrohyun <dev.electrohyun@gmail.com>
2025-09-18 17:02:12 +0900

    feat: (#4) 웰컴 페이지 작성

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router';
+import { motion } from 'framer-motion';
 
 const WelcomePage = () => {
   const navigate = useNavigate();
@@ -9,17 +10,23 @@ const WelcomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.4,
+          scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
+        }}
         onClick={goToLoginPage}
-        className="w-[300px] h-[300px] bg-white rounded-full shadow-md"
+        className="w-[250px] h-[250px] bg-white rounded-full shadow-md cursor-pointer"
       />
 
-      <div className="mt-10 text-center">
-        <span className="mt-6 text-4xl text-green-500">밥아저씨</span>
+      <div className="mt-10 text-center font-ssrm font-bold">
+        <span className="text-4xl text-green-500">밥아저씨</span>
         <span className="text-4xl">
           님<br />
         </span>
-        <span className="text-4xl">안녕하세요!</span>
+        <p className="mt-1 text-4xl">안녕하세요!</p>
       </div>
     </div>
   );
