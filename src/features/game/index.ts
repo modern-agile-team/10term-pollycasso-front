afb82a3ef34775ea79b834aca58bd3cad75b5404
electrohyun <dev.electrohyun@gmail.com>
2026-01-19 19:37:20 +0900

    feat: (#89) ShortcutGuide 컴포넌트 추가

--- src/features/game/index.ts
+++ src/features/game/index.ts
@@ -4,4 +4,5 @@ export { GameSubmitButton } from './ui/GameSubmitButton';
 export { GameTimer } from './ui/GameTimer';
 export { InventoryPanel } from './ui/InventoryPanel';
 export { PlayerSidebar } from './ui/PlayerSidebar';
+export { ShortcutGuide } from './ui/ShortcutGuide';
 export { ThemeSelector } from './ui/ThemeSelector';
