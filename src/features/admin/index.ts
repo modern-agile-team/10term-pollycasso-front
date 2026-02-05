a3c77d62270f3409514258c43fbbe93ec04e49a2
electrohyun <dev.electrohyun@gmail.com>
2026-02-05 13:53:46 +0900

    feat: (#149) 로그인 페이지 기능 추가

--- src/features/admin/index.ts
+++ src/features/admin/index.ts
@@ -3,3 +3,4 @@ export { AdminSidebar } from './ui/AdminSidebar';
 export { ReportDetail } from './ui/ReportDetail';
 export { GenericTable } from './ui/GenericTable';
 export { MatchDetail } from './ui/MatchDetail';
+export { useAdminKnock } from './model/useAdminKnock';
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,29 @@
 import title from '@/assets/title.svg';
 import { LoginForm, SignupLink, SocialLoginButtons } from '@/features/auth';
 import { AuthLayout } from '@/widgets/auth/ui/AuthLayout';
+import { useAdminKnock } from '@/features/admin';
 
 const LoginPage = () => {
+  const { handleKnock } = useAdminKnock();
+
   return (
-    <AuthLayout>
-      <img
-        src={title}
-        className="w-full max-w-[405px] mx-auto mt-20"
-        alt="폴리카소 로고"
+    <div className="relative">
+      <div
+        onClick={handleKnock}
+        className="absolute bg-black/10 top-0 right-0 w-[50px] h-[50px] z-[999] cursor-default"
       />
-      <LoginForm />
-      <SocialLoginButtons />
-      <SignupLink />
-    </AuthLayout>
+
+      <AuthLayout>
+        <img
+          src={title}
+          className="w-full max-w-[405px] mx-auto mt-20"
+          alt="폴리카소 로고"
+        />
+        <LoginForm />
+        <SocialLoginButtons />
+        <SignupLink />
+      </AuthLayout>
+    </div>
   );
 };
 
